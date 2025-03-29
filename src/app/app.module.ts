72187c41eeb6594488df9f58d17334d7278d4267
Daniel Chirinos <daniel.chirinos@bermann.cl>
2025-03-29 00:58:50 -0300

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
+import { AsdComponent } from './asd/asd/asd.component';
+import { Asd2Component } from './asd/asd2/asd2.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
+        AsdComponent,
+        Asd2Component,
     ],
   imports: [
         BrowserModule,
